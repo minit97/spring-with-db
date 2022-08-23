1e3ff087791c6fe53a82ea76a10046a65994ad03
박현민 <dndb4902@naver.com>
2022-08-23 19:08:36 +0900

    # 임시

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -49,7 +49,7 @@ public class MemberServiceV3_2 {
     private void release(Connection con) {
         if (con != null) {
             try {
-                con.setAutoCommit(true);    // 커넥션 풀 고려
+                con.setAutoCommit(true);    // 커넥션 풀  고려
                 con.close();
             } catch (Exception e) {
                 log.info("error", e);
