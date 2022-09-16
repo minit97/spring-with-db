efa233b888afe5872615d1e9b1eee9124563b6c9
박현민 <dndb4902@naver.com>
2022-09-16 18:15:30 +0900

    # @Transactional2

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,6 +47,9 @@ class ItemRepositoryTest {
 //        transactionManager.rollback(status);
     }
 
+    @Transactional
+    @Commit
+    @Rollback(false)
     @Test
     void save() {
         //given
