76fda77d0304147ab4f365aa39373e44873193a2
박현민 <dndb4902@naver.com>
2022-09-24 14:51:10 +0900

    # 트랜잭션 AOP 프록시 내부호출 - 해결

--- springtx/src/test/java/hello/springtx/apply/InternalCallV2Test.java
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV2Test.java
@@ -1,5 +1,6 @@
 package hello.springtx.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 
 @Slf4j
 @SpringBootTest
-public class InternalCallV1Test {
+public class InternalCallV2Test {
 
     @Autowired
     CallService callService;
@@ -21,39 +22,50 @@ public class InternalCallV1Test {
         log.info("callService class={}",callService.getClass());
     }
 
-    @Test
-    void internalCall() {
-        callService.internal();
-    }
-
     /**
-     * 트랜잭션이 적용이 안되는 경우 : 내부 호출
+     * 내부호출 해결 : 클래스 별도 분리
      */
     @Test
-    void externallCall() {
+    void externallCallV2() {
         callService.external();
     }
-
 
     @TestConfiguration
     static class InternalCallV1TestConfig {
         @Bean
         CallService callService(){
-            return new CallService();
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
         }
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     static class CallService {
+
+        private final InternalService internalService;
 
         public void external() {
             log.info("call external");
             printTxInfo();
-            internal();
+            internalService.internal();
         }
 
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+
+    static class InternalService {
         @Transactional
-        void internal() {
+        public void internal() {
             log.info("call internal");
             printTxInfo();
         }
